5b473550fae5112cef43bad329aafe8d997193fc
Taku <fk.53rk2@icloud.com>
2025-01-03 20:04:35 +0900

    本家を参考に作ってみたが、棒のグラデーションが汚かったり、DOPA!のロゴの大きさや位置が少し変なので、あとで改善する。今はほかの機能を作ることに専念しよう。

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -8,6 +8,7 @@ export const Footer = () => {
     <div>
     <Simg src={dopa_new_logo} />
     </div>
+    <Sdivflex>
     <ul>
      <Sli>
        カテゴリー 
@@ -34,6 +35,25 @@ export const Footer = () => {
       ユニオンアリーナ
      </Sli2>
     </ul>
+    <ul>
+     <Sli>
+       DOPA!について 
+     </Sli>
+     <Sli2>
+       利用規約
+     </Sli2>
+     <Sli2>
+       FAQ・お問い合わせ
+     </Sli2>
+     <Sli2>
+       プライバシーポリシー
+     </Sli2>
+     <Sli2>
+       特定商取引法に基づく表記
+     </Sli2>
+    </ul>
+    <p>©2023 DOPA all rights reserved</p>
+    </Sdivflex>
   </div>
     )
 }
@@ -41,21 +61,30 @@ const Sli = styled.li`
     list-style: none;
     font-size: 20px;
     font-weight: bold;
+    display: flex;
 `
 const Sli2 = styled.li`
     list-style: none;
     font-size: 20px;
     color: #dd3851e0;
+    &:hover{
+      text-decoration: underline;
+      color: #dd3851e0;
+    }
 `
 const Simg = styled.img`
     width: 150px;
     margin-left: 100px;
     padding: 25px;
 `
-
+const Sdivflex = styled.div`
+  display: flex;
+`
 const Scolorfulrod = styled.div`
     border-bottom: 5px solid;
     height: 300px;
     width: 80%;
     margin: 0 auto;
+    border-image: linear-gradient(to right, rgb(233, 70, 70), #1ed724d6) 1;
+  
 `
