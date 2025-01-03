e5ed64dfdb5dc83f34da00343202baf08d1fc53b
Taku <fk.53rk2@icloud.com>
2025-01-03 20:41:22 +0900

    Footer.tsxをインポートした

--- src/Components/Pages/dp.tsx
+++ src/Components/Pages/dp.tsx
@@ -1,8 +1,11 @@
 import React from "react"
+import { Footer } from "../Footer/Footer.tsx";
 export const DP = () => {
     return (
         <div>
            <p>DPページです</p> 
+
+           <Footer />
         </div>
     )
 };
--- src/Components/Pages/others.tsx
+++ src/Components/Pages/others.tsx
@@ -1,8 +1,11 @@
 import React from "react"
+import { Footer } from "../Footer/Footer.tsx";
 export const Others = () => {
     return (
         <div>
            <p>その他のページです</p> 
+
+           <Footer />
         </div>
     )
 };
