b0b15a193b680dc3069d3df08ff1cac760bab349
Taku <fk.53rk2@icloud.com>
2025-01-03 15:06:58 +0900

    ページの遷移先を設定した。

---
+++ src/Components/Router/PageRouter.jsx
@@ -0,0 +1,27 @@
+
+import {Routes,Route,} from "react-router-dom"
+import { Pokemon } from "../Pages/pokemon.tsx"
+import { OnePiece } from "../Pages/onepiece.tsx"
+import { Dragonball } from "../Pages/dragonball.tsx"
+import { Yuugiou } from "../Pages/yuugiou.tsx"
+import { Vice } from "../Pages/vice.tsx"
+import { DP } from "../Pages/dp.tsx"
+import {UnionArena} from "../Pages/unionarena.tsx"
+import { StepUp } from "../Pages/stepup.tsx"
+import { Others } from "../Pages/others.tsx"
+
+export const PageRouter = () => {
+   return (
+    <Routes >
+      <Route path="pokemon" element={<Pokemon />}/>
+      <Route path="onepiece" element={<OnePiece />}/>
+      <Route path="dragonball" element={<Dragonball />}/>
+      <Route path="yuugiou" element={<Yuugiou />}/>
+      <Route path="vice" element={<Vice />}/>
+      <Route path="unionarena" element={<UnionArena />}/>
+      <Route path="dp" element={<DP />}/>
+      <Route path="stepup" element={<StepUp />}/>
+      <Route path="others" element={<Others />}/>
+    </Routes>
+   )
+}
