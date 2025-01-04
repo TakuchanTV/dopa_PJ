02155577972b961bbcbc306f4e5d43d822cdb4e6
Taku <fk.53rk2@icloud.com>
2025-01-04 16:49:52 +0900

    アイコンをspanタグの中に埋め込んだ

--- src/Components/Header/Makingtab.tsx
+++ src/Components/Header/Makingtab.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import styles from "./Makingtab.module.css"
 import dopa_new_logo from "../Images/dopa_new_logo.png"
+import beginner from "../Images/beginner.png"
 import { useNavigate } from "react-router-dom"
 export const Makingtab = () => {
     const navigate = useNavigate();
@@ -8,6 +9,9 @@ export const Makingtab = () => {
     return(
       <div className={styles.tab}>
         <img src={dopa_new_logo} onClick={() => navigateHome()}/>
+        <span className={styles.icon}>
+          <img src={beginner} />  
+        </span>
       </div>  
     )
 }
