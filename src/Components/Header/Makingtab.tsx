ecda00fea5995d013deccdaf857e0985e3d670c2
Taku <fk.53rk2@icloud.com>
2025-01-04 17:38:42 +0900

    navigateを使って、ページ遷移ができるように設定したり、Makingtab.module.cssが読み取れるようにインポートした

--- src/Components/Header/Makingtab.tsx
+++ src/Components/Header/Makingtab.tsx
@@ -6,12 +6,20 @@ import { useNavigate } from "react-router-dom"
 export const Makingtab = () => {
     const navigate = useNavigate();
     const navigateHome = () => navigate("/")
+    const navigateCharge = () => navigate("/charge")
     return(
       <div className={styles.tab}>
         <img src={dopa_new_logo} onClick={() => navigateHome()}/>
         <span className={styles.icon}>
           <img src={beginner} />  
         </span>
+        <div className={styles.DP}>
+        <span >0DP</span>
+        </div>
+        <div className={styles.charge} onClick={() => navigateCharge()}>
+        <span className={styles.plus} >+</span>
+        <span >100PT</span>
+        </div>
       </div>  
     )
 }
