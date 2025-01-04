3f4c6fe8778b6ca652a1c0e7745087d1517ee492
Taku <fk.53rk2@icloud.com>
2025-01-04 20:45:48 +0900

    <Makingtab />と<Banner />を削除しました

--- src/Components/Pages/charge.tsx
+++ src/Components/Pages/charge.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Makingtab } from "../Header/Makingtab";
-import { Banner } from "../Header/Banner";
+
 export const Charge = () => {
     return (
         <div>
-            <Makingtab />
-             <Banner />
-            <p>Chargeページです</p>
+          <p>Chargeページです</p>
         </div>
     )
 }
