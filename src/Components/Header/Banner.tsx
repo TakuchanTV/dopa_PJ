d2a54282e76fe20a901ab180cb6dcdddbe21a977
Taku <fk.53rk2@icloud.com>
2025-01-04 09:51:27 +0900

    ./Banner.module.cssをインポートしてCSSを当てることができた。

--- src/Components/Header/Banner.tsx
+++ src/Components/Header/Banner.tsx
@@ -1,4 +1,11 @@
-export const Banner = () => {
+import React from "react"
+import styles from "./Banner.module.css"
 
+export const Banner = () => {
+return (
+   <div className={styles.color}>
+      <p>これは練習です。</p>
+   </div> 
+)
 
 }
