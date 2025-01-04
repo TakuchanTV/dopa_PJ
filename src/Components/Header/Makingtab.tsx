c552d9050c8f94e345b4bdc4b1f8a1565bbb2fe5
Taku <fk.53rk2@icloud.com>
2025-01-04 16:16:56 +0900

    ページの一番上に表示されているタブを作製した。左上にDOPA!アイコンを埋め込んだので、今度はユーザーのアイコンと課金ページをつくる

---
+++ src/Components/Header/Makingtab.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./Makingtab.module.css"
+import dopa_new_logo from "../Images/dopa_new_logo.png"
+import { useNavigate } from "react-router-dom"
+export const Makingtab = () => {
+    const navigate = useNavigate();
+    const navigateHome = () => navigate("/")
+    return(
+      <div className={styles.tab}>
+        <img src={dopa_new_logo} onClick={() => navigateHome()}/>
+      </div>  
+    )
+}
