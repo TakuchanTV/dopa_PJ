3b679b5160f376f8854ef3c27f5b29b54a2a53d6
Taku <fk.53rk2@icloud.com>
2025-01-04 11:07:47 +0900

    バナーの写真をインポートして挿入し、Banner.module.cssでバナーのデザインを調整している。

--- src/Components/Header/Banner.tsx
+++ src/Components/Header/Banner.tsx
@@ -1,10 +1,40 @@
 import React from "react"
 import styles from "./Banner.module.css"
-
+import bannerphoto1 from "../Images/バナーの写真/bannerphoto1.png"
+import bannerphoto2 from "../Images/バナーの写真/bannerphoto2.png"
+import bannerphoto3 from "../Images/バナーの写真/bannerphoto3.png"
+import bannerphoto4 from "../Images/バナーの写真/bannerphoto4.png"
+import bannerphoto5 from "../Images/バナーの写真/bannerphoto5.png"
 export const Banner = () => {
 return (
-   <div className={styles.color}>
-      <p>これは練習です。</p>
+    // スライドの外枠
+   <div className={styles.slidewrapper} >
+    <div className={styles.slide}>
+      <div>
+        <img src={bannerphoto1} className={styles.img} />
+      </div>
+      <div >
+        <img src={bannerphoto2} className={styles.img} />
+      </div>
+      <div >
+        <img src={bannerphoto3} className={styles.img} />
+      </div>
+      <div>
+        <img src={bannerphoto4}  className={styles.img}/>
+      </div>
+      <div >
+        <img src={bannerphoto5} className={styles.img} />
+      </div>
+    </div>
+     {/*左右のボタン */}
+     <span className={styles.prev}></span>
+     <span className={styles.next}></span>
+     {/*インジケーター*/}
+     <ul className={styles.indicator}>
+       <li className={styles.list}></li>
+       <li className={styles.list}></li>
+       <li className={styles.list}></li>
+     </ul>
    </div> 
 )
 
