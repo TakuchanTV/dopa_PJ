ddd8bff4dce0aefa93b472cb1754f484d94b4eb4
Taku <fk.53rk2@icloud.com>
2025-01-05 23:16:03 +0900

    インディケータのliタグをクリックしたところが黒くなり、それ以外は白くなるように書きたかったが、まだできていない。もう少しでできそう

--- src/Components/Header/Banner.tsx
+++ src/Components/Header/Banner.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react"
+import React, {useEffect, useRef, useState} from "react"
 import styles from "./Banner.module.css"
 import bannerphoto1 from "../Images/バナーの写真/bannerphoto1.png"
 import bannerphoto2 from "../Images/バナーの写真/bannerphoto2.png"
@@ -8,21 +8,23 @@ import bannerphoto5 from "../Images/バナーの写真/bannerphoto5.png"
 export const Banner = () => {
   const SlideLef = useRef<HTMLDivElement>(null)
   const ListsRef = useRef<HTMLLIElement>(null)
+  const [activeIndex, setActiveIndex] = useState<number>(0)
   const IndicatorRef = useRef<HTMLDivElement>(null)
-  let count:number = 0;
-  const slide = SlideLef.current;
   const lists = ListsRef.current;
   
   
 
   const handleClick = (i:number) => {
-    ListsRef.current?.focus();
+    setActiveIndex(i)
    if (SlideLef.current){
     SlideLef.current.className = `${styles.slide} ${styles[`slide${i}`]}`;
    }
-    
+  //  if (lists) {
+  //   lists.className = `${styles.list} ${styles[`li${i}`]}`
+  //  }
   }
 
+  
   
   
   
@@ -50,12 +52,13 @@ return (
      {/* <span className={styles.prev}></span>
      <span className={styles.next}></span> */}
      {/*インジケーター*/}
+     
      <ul className={styles.indicator} >
-       <li className={styles.list} ref={ListsRef} onClick={() =>handleClick(1)}></li>
-       <li className={styles.list} ref={ListsRef} onClick={() =>handleClick(2)} ></li>
-       <li className={styles.list} ref={ListsRef} onClick={() =>handleClick(3)}></li>
-       <li className={styles.list} ref={ListsRef} onClick={() =>handleClick(4)}></li>
-       <li className={styles.list} ref={ListsRef} onClick={() =>handleClick(5)}></li>
+       <li className={`${styles.list} ${activeIndex === activeIndex ? styles.active:styles.notactive}`} ref={ListsRef} onClick={() =>handleClick(1)}></li>
+       <li className={`${styles.list} ${activeIndex === activeIndex ? styles.active:styles.notactive}`} ref={ListsRef} onClick={() =>handleClick(2)} ></li>
+       <li className={`${styles.list} ${activeIndex === activeIndex ? styles.active:styles.notactive}`} ref={ListsRef} onClick={() =>handleClick(3)}></li>
+       <li className={`${styles.list} ${activeIndex === activeIndex ? styles.active:styles.notactive}`} ref={ListsRef} onClick={() =>handleClick(4)}></li>
+       <li className={`${styles.list} ${activeIndex === activeIndex? styles.active:styles.notactive}`} ref={ListsRef} onClick={() =>handleClick(5)}></li>
      </ul>
    </div> 
 )
