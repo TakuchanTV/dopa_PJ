a4053c155f625b466df7559138f9d0761777d98c
Taku <fk.53rk2@icloud.com>
2025-01-07 18:02:12 +0900

    onepiece.module.cssを読み込むために最後の一文を追加した

--- src/types/import-image.d.ts
+++ src/types/import-image.d.ts
@@ -2,3 +2,4 @@ declare module "*.png";
 declare module "*./Makingtab.module.css";
 declare module "*./Banner.module.css";
 declare module "*../ModuleCSS/pokemon.module.css"
+declare module"*../ModuleCSS/onepiece.module.css"
