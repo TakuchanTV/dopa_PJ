2d0e44bd737fd9d4e984e213d4ef42d9aed92658
Taku <fk.53rk2@icloud.com>
2025-01-07 18:24:03 +0900

    dragonball.module.cssを読み込むために追加した]

--- src/types/import-image.d.ts
+++ src/types/import-image.d.ts
@@ -1,5 +1,6 @@
 declare module "*.png";
 declare module "*./Makingtab.module.css";
 declare module "*./Banner.module.css";
-declare module "*../ModuleCSS/pokemon.module.css"
-declare module "*../ModuleCSS/onepiece.module.css"
+declare module "*../ModuleCSS/pokemon.module.css";
+declare module "*../ModuleCSS/onepiece.module.css";
+declare module "*../ModuleCSS/dragonball.module.css"
