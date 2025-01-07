d528b6bfe48689949c8fefb3e98d3f4c07385d8b
Taku <fk.53rk2@icloud.com>
2025-01-07 20:04:48 +0900

    vice.module.cssを読み込むために declare moduleを記述した

--- src/types/import-image.d.ts
+++ src/types/import-image.d.ts
@@ -3,4 +3,5 @@ declare module "*./Makingtab.module.css";
 declare module "*./Banner.module.css";
 declare module "*../ModuleCSS/pokemon.module.css";
 declare module "*../ModuleCSS/onepiece.module.css";
-declare module "*../ModuleCSS/dragonball.module.css"
+declare module "*../ModuleCSS/dragonball.module.css";
+declare module "*../ModuleCSS/vice.module.css";
