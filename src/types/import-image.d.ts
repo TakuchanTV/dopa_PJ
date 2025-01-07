52504f129870a37c7c67e83b4ddc56aec219571d
Taku <fk.53rk2@icloud.com>
2025-01-07 18:11:03 +0900

    コンフリクト解消したい

--- src/types/import-image.d.ts
+++ src/types/import-image.d.ts
@@ -3,4 +3,3 @@ declare module "*./Makingtab.module.css";
 declare module "*./Banner.module.css";
 declare module "*../ModuleCSS/pokemon.module.css"
 
-declare module "*../ModuleCSS/dragonball.module.css"
