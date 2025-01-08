7d5a20b3b233946201a00d2de5ee63c9543592b1
Taku <fk.53rk2@icloud.com>
2025-01-08 12:02:47 +0900

    others.module.cssを追加するために最後の一文を追加した

--- src/types/import-image.d.ts
+++ src/types/import-image.d.ts
@@ -5,4 +5,5 @@ declare module "*../ModuleCSS/pokemon.module.css";
 declare module "*../ModuleCSS/onepiece.module.css";
 declare module "*../ModuleCSS/dragonball.module.css";
 declare module "*../ModuleCSS/vice.module.css";
-declare module "*../ModuleCSS/unionarena.module.css"
+declare module "*../ModuleCSS/unionarena.module.css";
+declare module "*../ModuleCSS/others.module.css"
