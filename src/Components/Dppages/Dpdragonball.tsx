2a1589169eed373fbe66438352043ffb11f951c7
Taku <fk.53rk2@icloud.com>
2025-01-11 13:56:57 +0900

    それぞれのDPページの先に表示されるファイルを作った

---
+++ src/Components/Dppages/Dpdragonball.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Gachaddragonball } from '../Header/Navigatedp/Gachadpdragonball.tsx'
+export const Dpdragonball = () => {
+  return (
+    <div>
+        <Gachaddragonball />
+        <div>Dpdragonball</div>
+    </div>
+  )
+}
