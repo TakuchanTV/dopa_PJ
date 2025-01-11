ad7b59a6bb7aaefdb9c91bd9e967bffe59f9f599
Taku <fk.53rk2@icloud.com>
2025-01-11 14:40:04 +0900

    それぞれのページからDPページに遷移したときに表示される画面を作った

---
+++ src/Components/Dppages/Dpvice.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Gachadpvice } from '../Header/Navigatedp/Gachadpvice.tsx'
+
+export const Dpvice = () => {
+  return (
+   <div>
+       <Gachadpvice />
+       <div>Dpvice</div>
+   </div> 
+  )
+}
---
+++ src/Components/Dppages/Dpyuugiou.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Gachadpyuugiou } from '../Header/Navigatedp/Gachadpyuugiou.tsx'
+
+export const Dpyuugiou = () => {
+  return (
+   <div>
+    <Gachadpyuugiou />
+    <div>Dpyuugiou</div>
+   </div> 
+  )
+}
