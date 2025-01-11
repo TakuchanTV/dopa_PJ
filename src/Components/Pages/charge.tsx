a0bde75730a0d75f9118bc79fb3f831b85385494
Taku <fk.53rk2@icloud.com>
2025-01-11 19:46:24 +0900

    課金ページを模倣して作った

--- src/Components/Pages/charge.tsx
+++ src/Components/Pages/charge.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import styles from "../ModuleCSS/charge.module.css"
+import Coin from "../Images/Coin.png"
 
 export const Charge = () => {
     return (
-        <div>
-          <p>Chargeページです</p>
+        <div className={styles.charge}>
+          <p>ポイントを選択</p>
+          <br />
+          <span>クレジットカード、バーコード決済、デジタルウォレット、銀行振込等で決済が可能です。</span>
+          <div className={styles.yen60}>
+            <span className={styles.span1}>88%OFF
+            </span>
+            <span className={styles.span2}>
+              <span className={styles.span500}>500</span>
+              <span className={styles.point}>point</span>
+            <img src={Coin} />
+            </span>
+            <span className={styles.span3}>￥60</span>
+          </div>
         </div>
     )
 }
