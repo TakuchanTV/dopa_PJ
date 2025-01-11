265df36aa0c0778c9bbd79e693b7a435197129c0
Taku <fk.53rk2@icloud.com>
2025-01-11 13:02:50 +0900

    ポケモンページのガチャとDPのページを行き来できるようにこのファイルを作り、コンポーネントとして使いまわす

---
+++ src/Components/Header/Navigatedp/Gachadppokemon.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import styles from "../Navigatedp/Gachadp.module.css"
+import arrow from "../../Images/リサイクル矢印1.png"
+import { useNavigate } from 'react-router-dom'
+export const Gachadppokemon = () => {
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+      navigate("/pokemon")
+    }
+  const handleNavigatedp = () => {
+     navigate("/pokemon/dp")
+  }
+  return (
+    <div className={styles.gachadp} >
+        <p className={styles.gacha} onClick={handleNavigate}>ガチャの一覧
+          <span></span>  
+        </p>
+        <p className={styles.dp} onClick={handleNavigatedp} >DPを交換する
+           <img src={arrow}/> 
+        </p>
+    </div>
+  )
+}
+
