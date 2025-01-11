e7e63d2d897353813c3bd9813c50741e080fe393
Taku <fk.53rk2@icloud.com>
2025-01-11 14:44:12 +0900

    DPページに遷移できるようにRouteにパスとコンポーネントを登録した

--- src/Components/Router/PageRouter.jsx
+++ src/Components/Router/PageRouter.jsx
@@ -10,18 +10,32 @@ import {UnionArena} from "../Pages/unionarena.tsx"
 import { StepUp } from "../Pages/stepup.tsx"
 import { Others } from "../Pages/others.tsx"
 import { Charge } from "../Pages/charge.tsx"
+import { Dppokemon } from "../Dppages/Dppokemon.tsx"
+import { Dponepiece } from "../Dppages/Dponepiece.tsx"
+import { Dpdragonball } from "../Dppages/Dpdragonball.tsx"
+import { Dpyuugiou } from "../Dppages/Dpyuugiou.tsx"
+import { Dpvice } from "../Dppages/Dpvice.tsx"
+import { Dpunionarena } from "../Dppages/Dpunionarena.tsx"
+import { Dpothers } from "../Dppages/Dpothers.tsx"
 export const PageRouter = () => {
    return (
     <Routes >
       <Route path="pokemon" element={<Pokemon />}/>
+      <Route path="pokemon/dp" element={<Dppokemon />}/>
       <Route path="onepiece" element={<OnePiece />}/>
+      <Route path="onepiece/dp" element={<Dponepiece />}/>
       <Route path="dragonball" element={<Dragonball />}/>
+      <Route path="dragonball/dp" element={<Dpdragonball />}/>
       <Route path="yuugiou" element={<Yuugiou />}/>
+      <Route path="yuugiou/dp" element={<Dpyuugiou />}/>
       <Route path="vice" element={<Vice />}/>
+      <Route path="vice/dp" element={<Dpvice />}/>
       <Route path="unionarena" element={<UnionArena />}/>
+      <Route path="unionarena/dp" element={<Dpunionarena />}/>
       <Route path="dp" element={<DP />}/>
       <Route path="stepup" element={<StepUp />}/>
       <Route path="others" element={<Others />}/>
+      <Route path="others/dp" element={<Dpothers />}/>
       <Route path="charge" element={<Charge />}/>
     </Routes>
    )
