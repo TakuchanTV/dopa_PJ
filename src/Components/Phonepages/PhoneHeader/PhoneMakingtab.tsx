6a9c0c94a4e87de018d0e2f21f5d70b600900ed4
Taku <fk.53rk2@icloud.com>
2025-01-18 11:21:32 +0900

    スマートフォンの時に表示される画面を設定した

---
+++ src/Components/Phonepages/PhoneHeader/PhoneMakingtab.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from "../PhoneModuleCSS/PhoneMakingtab.module.css"
+import dopa_new_logo from "../../Images/dopa_new_logo.png"
+import beginner from "../../Images/beginner.png"
+import { useNavigate } from "react-router-dom"
+
+export const PhoneMakingtab = () => {
+    const navigate = useNavigate();
+    const navigateHome = () => navigate("/")
+    const navigateCharge = () => navigate("/charge")
+  return (
+    <div className={styles.tab}>
+        <img src={dopa_new_logo} onClick={() => navigateHome()}/>
+        <span className={styles.icon}>
+          <img src={beginner} />  
+        </span>
+        <div className={styles.DP}>
+        <span >0DP</span>
+        </div>
+        <div className={styles.charge} onClick={() => navigateCharge()}>
+        <span className={styles.plus} >+</span>
+        <span >100PT</span>
+        </div>
+      </div>  
+  )
+}
+
