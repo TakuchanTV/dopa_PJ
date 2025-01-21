002af324c3df34314ed1ca947010c2adeb92ebf4
Taku <fk.53rk2@icloud.com>
2025-01-21 18:53:31 +0900

    レスポンシブ対応にするためにこのファイルを作った

---
+++ src/Components/Phonepages/PhoneHeader/Navigatedp/PhoneGachadponepiece.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import styles from "../Navigatedp/PhoneGachadponepiece.module.css"
+import arrow from "../../../Images/リサイクル矢印1.png"
+
+export const PhoneGachadponepiece = () => {
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+      navigate("/onepiece")
+    }
+  const handleNavigatedp = () => {
+     navigate("/onepiece/dp")
+  }
+  return (
+    <div className={styles.gachadp} >
+        <p className={styles.gacha} onClick={handleNavigate}>ガチャの一覧
+        <span></span>  
+        </p>
+        <p className={styles.dp} onClick={handleNavigatedp} >DPを交換する
+        <img src={arrow}/> 
+        </p>
+    </div>
+  )
+}
+
