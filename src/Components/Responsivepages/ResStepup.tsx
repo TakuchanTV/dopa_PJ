cf8710e1c2020679ac8681189ced3ef72b81b95d
Taku <fk.53rk2@icloud.com>
2025-01-25 21:17:19 +0900

    確認のためこのファイルにPopupコンポーネントが表示できるようにした

--- src/Components/Responsivepages/ResStepup.tsx
+++ src/Components/Responsivepages/ResStepup.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useMediaQuery } from 'react-responsive'
 import { Loadingsquares } from '../Loading/Loadingsquares.tsx'
 import { Loadingdots } from '../Loading/Loadingdots.tsx'
+import { Popup } from '../Popup/Popup.js'
 
 export const ResStepup = () => {
   const isDesktop: boolean = useMediaQuery({query: `(min-width: 414px)`})  
   return (
     <div>
       {/*414px以上はデスクトップの画面*/}  
-      {isDesktop && <Loadingsquares />}
+      {isDesktop && <Popup />}
       {/*414px未満はモバイル用の画面*/}  
       {!isDesktop && <Loadingdots />}
     </div>
