2b310c60ac8e60b0c20b18322f5b70de071fc8f1
Taku <fk.53rk2@icloud.com>
2025-01-28 15:28:43 +0900

    ボタンをクリックすると表示されるポップアップを作った

--- src/Components/Popup/Popup10.js
+++ src/Components/Popup/Popup10.js
@@ -2,23 +2,23 @@ import React, { useState } from 'react'
 import styles from "../Popup/Popup.module.css"
 import { useNavigate } from 'react-router-dom'
 
-export const Popup = ({popupvisible,togglePopup}) => {
+export const Popup10 = ({popupvisible10,togglePopup10}) => {
 // const [popupvisible, setPopupvisible] = useState(false)
 
 // const togglePopup = () => setPopupvisible(!popupvisible)
 const navigate = useNavigate()
 
-const checkfunc = () =>navigate("/")
+const checkfunc = () =>navigate("/onepiece")
   return (
     <div>
       {/* <button onClick={togglePopup}>ポップアップを開く</button> */}
-      {popupvisible && (
+      {popupvisible10 && (
         <div className={styles.Popup}>
          {/*ポップアップの画面*/}
          <p>ポップアップの画面を表示します</p>
           <div className={styles.btndiv}>
-            <button onClick={checkfunc} className={styles.btn1}>ガチャを引く</button>
-            <button onClick={togglePopup} className={styles.btn2}>キャンセル</button>
+            <button onClick={checkfunc} className={styles.btn1}>10連ガチャを引く</button>
+            <button onClick={togglePopup10} className={styles.btn2}>キャンセル</button>
           </div>
         </div>
       )}  
