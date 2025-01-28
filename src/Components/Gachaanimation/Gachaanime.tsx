26edc5f2331f7c035e9c3cb55380864a4a43e4e5
Taku <fk.53rk2@icloud.com>
2025-01-29 00:39:43 +0900

    ガチャを回すアニメーションを作りたい

--- src/Components/Gachaanimation/Gachaanime.tsx
+++ src/Components/Gachaanimation/Gachaanime.tsx
@@ -6,22 +6,39 @@ import anime from "animejs"
 export const Gachaanime = () => {
 const elems = useRef<(HTMLDivElement)>(null)
 useEffect(() => {
-  
-        if (elems.current) {
-            elems.current.addEventListener("click",() => {
+  const autoPlay = setInterval(() => {
+    if (elems.current) {
+        anime({
+           targets:elems.current,
+           translateY:450,
+           backgroundColor:"#ebe830",
+           borderRadius: "100px",
+           opacity:1,
+           complete: () => {
             anime({
-             targets:elems.current,
-             translateY:250,
-             backgroundColor:"#ebe830",
-             borderRadius: "100px",
-             duration:3000,
+                targets:elems.current,
+                translateY:0,
+                backgroundColor:"rgb(90, 135, 255)",
+                opacity:0,
             })
-            })
-        }
+           }
+        })
+    }
+  },4000)
+  return  () => clearInterval(autoPlay);
+        // if (elems.current) {
+        //     elems.current.addEventListener("load",() => {
+        //     anime({
+        //      targets:elems.current,
+        //      translateY:450,
+        //      backgroundColor:"#ebe830",
+        //      borderRadius: "100px",
+        //      duration:3000,
+        //     })
+        //     })
+        // }
 
     },[])
-    
-
 
   return (
     <div className={styles.wrapper}>
