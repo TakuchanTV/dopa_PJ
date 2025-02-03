93744e461494505bd1653481b8d520eb06f9d9e7
Taku <fk.53rk2@icloud.com>
2025-02-03 18:54:40 +0900

    ガチャを回すときのアニメーションを再現しようとした

--- src/Components/Gachaanimation/Gachaanime.tsx
+++ src/Components/Gachaanimation/Gachaanime.tsx
@@ -1,48 +1,43 @@
 import React, {useEffect, useRef} from 'react'
 import styles from "./GachaanimeCSS/Gachaanime.module.css"
 import anime from "animejs"
+import skyblue from "./GachaImages/skyblue.png"
+import { keyframes } from 'styled-components'
 
 
 export const Gachaanime = () => {
 const elems = useRef<(HTMLDivElement)>(null)
+
 useEffect(() => {
   const autoPlay = setInterval(() => {
     if (elems.current) {
         anime({
            targets:elems.current,
            translateY:450,
-           backgroundColor:"#ebe830",
            borderRadius: "100px",
            opacity:1,
-           complete: () => {
-            anime({
-                targets:elems.current,
-                translateY:0,
-                backgroundColor:"rgb(90, 135, 255)",
-                opacity:0,
-            })
-           }
-        })
+           duration:2000,
+   
+            complete:() => {
+              setTimeout(() => {
+                anime({
+                  targets:elems.current,
+                })  
+              },5000);
+            }
+            
+          })
     }
-  },4000)
+  },3000)
   return  () => clearInterval(autoPlay);
-        // if (elems.current) {
-        //     elems.current.addEventListener("load",() => {
-        //     anime({
-        //      targets:elems.current,
-        //      translateY:450,
-        //      backgroundColor:"#ebe830",
-        //      borderRadius: "100px",
-        //      duration:3000,
-        //     })
-        //     })
-        // }
-
+      
     },[])
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.square} ref={elems}></div>  
-    </div>
+    <div className={styles.div}>
+        <div className={styles.wrapper}>
+        <div className={styles.square} ref={elems}></div>  
+        </div>
+    </div> 
   )
 }
