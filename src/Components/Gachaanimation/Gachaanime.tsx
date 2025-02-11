b01bef7344e83d7e30d492b0582b60082044d887
Taku <fk.53rk2@icloud.com>
2025-02-12 00:57:46 +0900

    はずれ、当たり、大当たりの文章を一定の確率で表示できるようにした

--- src/Components/Gachaanimation/Gachaanime.tsx
+++ src/Components/Gachaanimation/Gachaanime.tsx
@@ -1,6 +1,7 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import styles from "./GachaanimeCSS/Gachaanime.module.css"
 import anime from "animejs"
+import {useNavigate } from 'react-router-dom'
 
 
 export const Gachaanime = () => {
@@ -13,9 +14,14 @@ const HitRef3 = useRef<(HTMLParagraphElement)>(null)
 const MissRef1 = useRef<(HTMLParagraphElement)>(null)
 const MissRef2 = useRef<(HTMLParagraphElement)>(null)
 const MissRef3 = useRef<(HTMLParagraphElement)>(null)
+const BigWinRef1 = useRef<(HTMLParagraphElement)>(null)
+const BigWinRef2 = useRef<(HTMLParagraphElement)>(null)
+const BigWinRef3 = useRef<(HTMLParagraphElement)>(null)
+const BigWinRef4 = useRef<(HTMLParagraphElement)>(null)
 const WrapperRef = useRef<(HTMLDivElement)>(null)
 
-
+const [randomText] = useState(() => Math.floor(Math.random()* 100));
+const navigate = useNavigate()
 
 useEffect(() => {
   const autoPlay = setTimeout(() => {
@@ -28,45 +34,84 @@ useEffect(() => {
       })
     }},3000)
 
-   const autoHitDisplay = setTimeout(() => {
-    if (HitRef1.current) {
-      HitRef1.current.classList.add(styles.hittextAnimation)
+    let autoDisplay;
+
+    if (randomText < 40) {
+      autoDisplay = setTimeout(() => {
+       if (HitRef1.current) {
+         HitRef1.current.classList.add(styles.hittextAnimation)
+       }
+       if (HitRef2.current) {
+         setTimeout(() => {
+           HitRef2.current?.classList.add(styles.hittextAnimation)
+         },500)
+       }
+       if (HitRef3.current) {
+         setTimeout(()=> {
+           HitRef3.current?.classList.add(styles.hittextAnimation)
+         },1000)
+       }
+       setTimeout(()=> {
+        navigate("/")
+       },11000)
+      },13000)
     }
-    if (HitRef2.current) {
-      setTimeout(() => {
-        HitRef2.current?.classList.add(styles.hittextAnimation)
-      },500)
+    else if (randomText >=40 && randomText < 90) {
+      autoDisplay = setTimeout(() => {
+       if (MissRef1.current) {
+         MissRef1.current.classList.add(styles.misstextAnimation)
+       }
+       if (MissRef2.current) {
+         setTimeout(() => {
+           MissRef2.current?.classList.add(styles.misstextAnimation)
+         },500)
+       }
+       if (MissRef3.current) {
+         setTimeout(()=> {
+           MissRef3.current?.classList.add(styles.misstextAnimation)
+         },1000)
+       }
+         setTimeout(()=> {
+          navigate("/")
+         },11000)
+      },13000)
     }
-    if (HitRef3.current) {
-      setTimeout(()=> {
-        HitRef3.current?.classList.add(styles.hittextAnimation)
-      },1000)
-    }
-   },13000)
-  //  const autoMissDisplay = setTimeout(() => {
-  //   if (MissRef1.current) {
-  //     MissRef1.current.classList.add(styles.misstextAnimation)
-  //   }
-  //   if (MissRef2.current) {
-  //     setTimeout(() => {
-  //       MissRef2.current?.classList.add(styles.misstextAnimation)
-  //     },500)
-  //   }
-  //   if (MissRef3.current) {
-  //     setTimeout(()=> {
-  //       MissRef3.current?.classList.add(styles.misstextAnimation)
-  //     },1000)
-  //   }
-  //  },13000)
 
   
-  return  () => {
+   else {
+     autoDisplay = setTimeout(() => {
+       if (BigWinRef1.current) {
+         BigWinRef1.current.classList.add(styles.bigwintextAnimation)
+       }
+       if (BigWinRef2.current) {
+         setTimeout(() => {
+           BigWinRef2.current?.classList.add(styles.bigwintextAnimation)
+         },500)
+       }
+       if (BigWinRef3.current) {
+         setTimeout(()=> {
+           BigWinRef3.current?.classList.add(styles.bigwintextAnimation)
+         },1000)
+       }
+       if (BigWinRef4.current) {
+         setTimeout(()=> {
+           BigWinRef4.current?.classList.add(styles.bigwintextAnimation)
+         },1500)
+       }
+       setTimeout(()=> {
+        navigate("/")
+       },11000)
+      },13000)
+   }
+
+  
+  return () => {
     clearTimeout(autoPlay);
-    clearTimeout(autoHitDisplay);
-    // clearTimeout(autoMissDisplay)
+    if (autoDisplay) {
+      clearInterval(autoDisplay)
+    }
   }
-    
-    },[])
+    },[randomText])
 
   return (
       <div className={styles.div}>
@@ -79,6 +124,10 @@ useEffect(() => {
           <p className={styles.misstext} ref={MissRef1}>は</p>
           <p className={styles.misstext} ref={MissRef2}>ず</p>
           <p className={styles.misstext} ref={MissRef3}>れ</p>
+          <p className={styles.bigwintext} ref={BigWinRef1}>大</p>
+          <p className={styles.bigwintext} ref={BigWinRef2}>当</p>
+          <p className={styles.bigwintext} ref={BigWinRef3}>た</p>
+          <p className={styles.bigwintext} ref={BigWinRef4}>り</p>
        </div>
     </div> 
   )
